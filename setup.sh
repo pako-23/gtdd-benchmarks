220847a116fcb94adcb20e5d01740e6ca88b3dc9
pasquale <pasqualepolverino98@gmail.com>
2023-09-28 11:52:39 +0200

    fix setup script

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+set -u
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt-get install -y ca-certificates curl gnupg golang make
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt-get install -y  docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker "$(whoami)"
