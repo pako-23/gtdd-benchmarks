f2fbca99874dafac70b395d5378f04ba97d86b3b
pasquale <pasqualepolverino98@gmail.com>
2023-09-28 15:14:13 +0200

    small fix

--- run-experiments.sh
+++ run-experiments.sh
@@ -3,67 +3,80 @@
 set -u
 
 RUNNERS=4
-
-# TODO: refactor this part
 CURR_DIR="$PWD"
 
+run_testsuite() {
+  local testsuite="$1"
+  local runners="${2:-4}"
+  local graph="$3"
+
+  for i in $(seq 1 3); do
+    "$GTDD_EXEC" run -v app_url=http://app \
+      -v driver_url=http://selenium:4444 -r "$runners" \
+      -i "$graph" -d selenium=selenium/standalone-chrome:115.0 \
+      "testsuites/$testsuite"
+    if [ "$?" -eq  0 ]; then
+      return 0
+    fi
+  done
+
+  return 1
+}
+
+run_experiment() {
+  local testsuite="$1"
+
+  if ! "$GTDD_EXEC" build "testsuites/$testsuite"; then
+    return
+  fi
+
+  if ! run_testsuite "$testsuite" '1'; then
+    return
+  fi
+
+  mkdir -p "results/$testsuite"
+
+  for i in $(seq 1 10); do
+    TIME="$(date  +"%d-%m-%y-%H-%M-%S")"
+    LOG_FILE="results/$testsuite/log-$TIME.json"
+    GRAPH_FILE="results/$testsuite/graph-$TIME.json"
+    SCHEDULES_FILE="results/$testsuite/schedules-$TIME.json"
+
+    "$GTDD_EXEC" deps --log debug --format json --log-file "$LOG_FILE" \
+      -v app_url=http://app \
+      -v driver_url=http://selenium:4444 \
+      -o "$GRAPH_FILE" -r "$RUNNERS" -s ex-linear \
+      -d selenium=selenium/standalone-chrome:115.0 \
+      "testsuites/$testsuite"
+    if [ "$?" -ne  0 ]; then
+      continue
+    fi
+
+    if ! run_testsuite "$testsuite" "$RUNNERS" "$GRAPH_FILE"; then
+      return
+    fi
+
+    "$GTDD_EXEC" schedules -i "$GRAPH_FILE" \
+      -o "$SCHEDULES_FILE" \
+      "testsuites/$testsuite"
+
+    ./compute-stats.py "$LOG_FILE" "$SCHEDULES_FILE" "results/$testsuite/stats.csv"
+  done
+}
+
+
+
+
+# TODO: refactor this part
 cd ../gtdd
 make
 
 GTDD_EXEC="../gtdd/gtdd"
-
 cd "$CURR_DIR"
 #######
-
-
-
-
-
 
 mkdir -p results
 
 for testsuite in $(ls testsuites | grep ppma); do
-  if ! "$GTDD_EXEC" build "testsuites/$testsuite"; then
-    continue
-  fi
-
-  "$GTDD_EXEC" run -v app_url=http://app \
-    -v driver_url=http://selenium:4444 -r 1 \
-    -d selenium=selenium/standalone-chrome:115.0 \
-    "testsuites/$testsuite"
-  if [ "$?" -ne  0 ]; then
-    continue
-  fi
-
-  mkdir -p "results/$testsuite"
-  TIME="$(date  +"%d-%m-%y-%H-%M-%S")"
-  LOG_FILE="results/$testsuite/log-$TIME.json"
-  GRAPH_FILE="results/$testsuite/graph-$TIME.json"
-  SCHEDULES_FILE="results/$testsuite/schedules-$TIME.json"
-
-  "$GTDD_EXEC" deps --log debug --format json --log-file "$LOG_FILE" \
-    -v app_url=http://app \
-    -v driver_url=http://selenium:4444 \
-    -o "$GRAPH_FILE" -r "$RUNNERS" -s ex-linear \
-    -d selenium=selenium/standalone-chrome:115.0 \
-    "testsuites/$testsuite"
-  if [ "$?" -ne  0 ]; then
-    continue
-  fi
-
-  "$GTDD_EXEC" run -v app_url=http://app \
-    -v driver_url=http://selenium:4444 \
-    -i "$GRAPH_FILE" -r "$RUNNERS" \
-    -d selenium=selenium/standalone-chrome:115.0 \
-    "testsuites/$testsuite"
-  if [ "$?" -ne  0 ]; then
-    continue
-  fi
-
-
-  "$GTDD_EXEC" schedules -i "$GRAPH_FILE" \
-    -o "$SCHEDULES_FILE" \
-    "testsuites/$testsuite"
-
-  ./compute-stats.py "$LOG_FILE" "$SCHEDULES_FILE" "results/$testsuite/stats.csv"
+  run_experiment "$testsuite"
 done
