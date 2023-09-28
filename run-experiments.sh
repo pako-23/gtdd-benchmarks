4bbd2441f1ed4c5f675b7aa41b7210aa7c87a976
pasquale <pasqualepolverino98@gmail.com>
2023-09-28 13:57:41 +0200

    test

--- run-experiments.sh
+++ run-experiments.sh
@@ -2,19 +2,18 @@
 
 set -u
 
+RUNNERS=4
+
 # TODO: refactor this part
 CURR_DIR="$PWD"
 
-cd /tmp
-
-cd ..
+cd ../gtdd
 make
-mv gtdd "$CURR_DIR"
+
+GTDD_EXEC="../gtdd/gtdd"
+
 cd "$CURR_DIR"
 #######
-
-
-
 
 
 
@@ -24,11 +23,11 @@ cd "$CURR_DIR"
 mkdir -p results
 
 for testsuite in $(ls testsuites | grep ppma); do
-  if ! ./gtdd build "testsuites/$testsuite"; then
+  if ! "$GTDD_EXEC" build "testsuites/$testsuite"; then
     continue
   fi
 
-  ./gtdd run -v app_url=http://app \
+  "$GTDD_EXEC" run -v app_url=http://app \
     -v driver_url=http://selenium:4444 -r 1 \
     -d selenium=selenium/standalone-chrome:115.0 \
     "testsuites/$testsuite"
@@ -42,19 +41,19 @@ for testsuite in $(ls testsuites | grep ppma); do
   GRAPH_FILE="results/$testsuite/graph-$TIME.json"
   SCHEDULES_FILE="results/$testsuite/schedules-$TIME.json"
 
-  ./gtdd deps --log debug --format json --log-file "$LOG_FILE" \
+  "$GTDD_EXEC" deps --log debug --format json --log-file "$LOG_FILE" \
     -v app_url=http://app \
     -v driver_url=http://selenium:4444 \
-    -o "$GRAPH_FILE" -r 8 -s ex-linear \
+    -o "$GRAPH_FILE" -r "$RUNNERS" -s ex-linear \
     -d selenium=selenium/standalone-chrome:115.0 \
     "testsuites/$testsuite"
   if [ "$?" -ne  0 ]; then
     continue
   fi
 
-  ./gtdd run -v app_url=http://app \
+  "$GTDD_EXEC" run -v app_url=http://app \
     -v driver_url=http://selenium:4444 \
-    -i "$GRAPH_FILE" -r 8 \
+    -i "$GRAPH_FILE" -r "$RUNNERS" \
     -d selenium=selenium/standalone-chrome:115.0 \
     "testsuites/$testsuite"
   if [ "$?" -ne  0 ]; then
@@ -62,7 +61,7 @@ for testsuite in $(ls testsuites | grep ppma); do
   fi
 
 
-  ./gtdd schedules -i "$GRAPH_FILE" \
+  "$GTDD_EXEC" schedules -i "$GRAPH_FILE" \
     -o "$SCHEDULES_FILE" \
     "testsuites/$testsuite"
 
