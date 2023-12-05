280bb31e22f10c1d0ba94902001f1120a3b63867
pasquale <pasqualepolverino98@gmail.com>
2023-12-05 14:21:58 +0100

    testing

--- run-experiments.sh
+++ run-experiments.sh
@@ -13,7 +13,7 @@ run_testsuite() {
   for i in $(seq 1 3); do
     "$GTDD_EXEC" run -v app_url=http://app \
       -v driver_url=http://selenium:4444 -r "$runners" \
-      -i "$graph" -d ./driver.yaml \
+      -i "$graph" -d ./selenium-driver.yaml \
       "testsuites/$testsuite" >> "$EXPERIMENT_LOGS" 2>&1
     if [ "$?" -eq  0 ]; then
       return 0
@@ -23,10 +23,41 @@ run_testsuite() {
   return 1
 }
 
-run_experiment() {
+single_iteration() {
   local testsuite="$1"
   local strategy="$2"
   local runners="12"
+
+  local TIME="$(date  +"%d-%m-%y-%H-%M-%S")"
+  local LOG_FILE="results/$testsuite/log-$strategy-$TIME.json"
+  local GRAPH_FILE="results/$testsuite/graph-$strategy-$TIME.json"
+  local SCHEDULES_FILE="results/$testsuite/schedules-$strategy-$TIME.json"
+
+  "$GTDD_EXEC" deps --log debug --format json --log-file "$LOG_FILE" \
+    -v app_url=http://app \
+    -v driver_url=http://selenium:4444 \
+    -o "$GRAPH_FILE" -r "$runners" -s "$strategy" \
+    -d ./selenium-driver.yaml \
+    "testsuites/$testsuite" >> "$EXPERIMENT_LOGS" 2>&1
+  if [ "$?" -ne  0 ]; then
+    return
+  fi
+
+  "$GTDD_EXEC" schedules -i "$GRAPH_FILE" \
+    -o "$SCHEDULES_FILE" \
+    "testsuites/$testsuite"
+
+  if ! run_testsuite "$testsuite" "$(nproc --all)" "$GRAPH_FILE"; then
+    echo "Schedule $SCHEDULES_FILE does not work" >> "$EXPERIMENT_LOGS"
+  fi
+
+  ./compute-stats.py "$LOG_FILE" \
+    "$SCHEDULES_FILE" \
+    "results/$testsuite/stats-$strategy.csv"  >> "$EXPERIMENT_LOGS" 2>&1
+}
+
+run_experiment() {
+  local testsuite="$1"
 
   if ! "$GTDD_EXEC" build "testsuites/$testsuite" >> "$EXPERIMENT_LOGS" 2>&1; then
     return
@@ -41,36 +72,10 @@ run_experiment() {
   fi
 
   for i in $(seq 1 10); do
-    TIME="$(date  +"%d-%m-%y-%H-%M-%S")"
-    LOG_FILE="results/$testsuite/log-$strategy-$TIME.json"
-    GRAPH_FILE="results/$testsuite/graph-$strategy-$TIME.json"
-    SCHEDULES_FILE="results/$testsuite/schedules-$strategy-$TIME.json"
-
-    "$GTDD_EXEC" deps --log debug --format json --log-file "$LOG_FILE" \
-      -v app_url=http://app \
-      -v driver_url=http://selenium:4444 \
-      -o "$GRAPH_FILE" -r "$runners" -s "$strategy" \
-      -d ./driver.yaml \
-      "testsuites/$testsuite" >> "$EXPERIMENT_LOGS" 2>&1
-    if [ "$?" -ne  0 ]; then
-      continue
-    fi
-
-    "$GTDD_EXEC" schedules -i "$GRAPH_FILE" \
-      -o "$SCHEDULES_FILE" \
-      "testsuites/$testsuite"
-
-    if ! run_testsuite "$testsuite" "$(nproc --all)" "$GRAPH_FILE"; then
-      echo "Schedule $SCHEDULES_FILE does not work" >> "$EXPERIMENT_LOGS"
-    fi
-
-    ./compute-stats.py "$LOG_FILE" \
-      "$SCHEDULES_FILE" \
-      "results/$testsuite/stats-$strategy.csv"  >> "$EXPERIMENT_LOGS" 2>&1
+    single_iteration "$testsuite" 'pradet'
+    single_iteration "$testsuite" 'pfast'
   done
 }
-
-
 
 
 # TODO: refactor this part
@@ -88,5 +93,5 @@ for testsuite in $(ls ./testsuites); do
   if [ -f "./results/$testsuite/stats-$1.csv" ]; then
     continue
   fi
-  run_experiment "$testsuite" "$1"
+  run_experiment "$testsuite"
 done
